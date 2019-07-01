5807a152a7e4860abc8aea834333851611308c4d
ahmed <sobh6081@gmail.com>
2019-07-02 01:03:59 +0200

    Updates

---
+++ precache-manifest.a5aafc138f24fce9cc677420ad46b71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85b81b317368a59b4bf199b96dcb64d",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d6f50fe36defd9bf53de",
+    "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "00132a1a70c5eaa5fbcd",
+    "url": "/contactmanager/static/css/main.07d815d3.chunk.css"
+  },
+  {
+    "revision": "d6f50fe36defd9bf53de",
+    "url": "/contactmanager/static/js/2.0251b094.chunk.js"
+  },
+  {
+    "revision": "00132a1a70c5eaa5fbcd",
+    "url": "/contactmanager/static/js/main.4e5c7f55.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
